bddc4418a530b75800aef69b478072dbaf6ae37f
Taweechai Yuenyang <krumii.it@gmail.com>
2021-12-28 15:36:21 +0700

    ทำ package

---
+++ cryex/bitkub.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+class Bitkub:
+    def __init__(self):
+        print(f"start bitkub")
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+import sys
+from .cryex.bitkub import Bitkub
 
+bitkub = Bitkub()
+
+def main():
+    sys.exit(0)
+    
+if __name__ == '__main__':
+    main()
+    
