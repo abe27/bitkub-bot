89174414d569de508b89c55e05fb0f99d54bcf3d
Taweechai Yuenyang <krumii.it@gmail.com>
2021-12-29 15:56:28 +0700

    update bitkub lib

--- crytoex/bitkub.py
+++ crytoex/bitkub.py
@@ -8,27 +8,40 @@ import json
 import requests
 import datetime
 import os
-import sys
 import cowsay
 from termcolor import colored
 
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+# import firebase_admin
+# from firebase_admin import credentials
+# from firebase_admin import db
 
 # set plotting with plotly
 pd.options.plotting.backend = "plotly"
 
+char_length = 60
+
 
 class Bitkub:
     def __init__(self):
-        cowsay.cow("""Bitkub Lib v.1\n\nCreate By: F San\nE-Mail: krumii.it@gmail.com\nGitHub: https://github.com/abe27""")
-        # Fetch the service account key JSON file contents
-        cred = credentials.Certificate('keys.json')
-        # Initialize the app with a service account, granting admin privileges
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': os.getenv('API_FIREBASE_URL')
-        })
+        """
+        Library นี้สร้างขึ้นมาเพื่อความต้องการส่วนตัว
+        """
+        print(u"{}[2J{}[;H".format(chr(27), chr(27)))
+        print(f"\n{colored(''.rjust(char_length, '*'), 'red')}")
+        cowsay.cow(colored("""
+                           😚 F-San Bitkub Lib v.1 🥰
+                           CreateBy 👻: F San
+                           E-Mail 💌: krumii.it@gmail.com
+                           GitHub 🌤: https://github.com/abe27
+                           License: MIT
+                           """, "blue"))
+        print(f"\n{colored(''.rjust(char_length, '*'), 'red')}")
+        # # Fetch the service account key JSON file contents
+        # cred = credentials.Certificate('keys.json')
+        # # Initialize the app with a service account, granting admin privileges
+        # firebase_admin.initialize_app(cred, {
+        #     'databaseURL': os.getenv('API_FIREBASE_URL')
+        # })
 
         # API info
         self.API_HOST = os.getenv('API_BITKUB_HOST')
@@ -46,13 +59,14 @@ class Bitkub:
             'X-BTK-APIKEY': self.API_KEY,
         }
         
-    @property
-    def api_key(self):
-        return self.API_KEY
+    # @property
+    # def api_key(self):
+    #     return self.API_KEY
 
     @staticmethod
     def __json_encode(data):
         return json.dumps(data, separators=(',', ':'), sort_keys=True)
+
 
     def sign(self, data):
         j = self.__json_encode(data)
@@ -81,20 +95,30 @@ class Bitkub:
         obj = json.loads(response.text)
 
         data = [{
-            'available': obj['result'][self.API_CURRENCY]['available'],
+            'available': float(obj['result'][self.API_CURRENCY]['available']),
             'reserved': obj['result'][self.API_CURRENCY]['reserved'],
             'symbol': self.API_CURRENCY
         }]
+        
         if obj['error'] == 0:
             currency = obj['result']
             for i in currency:
                 if currency[i]['reserved'] > 0:
                     currency[i]['symbol'] = i
                     data.append(currency[i])
+                    
+        msg = f"""\n
+        Available: {obj['result'][self.API_CURRENCY]['available']}
+        Reserved: {obj['result'][self.API_CURRENCY]['reserved']}
+        Symbol: {self.API_CURRENCY}
+        """
+        # print(u"{}[2J{}[;H".format(chr(27), chr(27)))
+        print(colored(msg, 'green'))
+        print(''.rjust(char_length, '-') + "\n")
 
         return data
 
-    def get_symbols(self):
+    def get_assets(self):
         response = requests.get(
             self.API_HOST + "/api/market/symbols", headers={}, data={})
         obj = json.loads(response.text)
@@ -121,6 +145,75 @@ class Bitkub:
             price = rticker[symbol]
             return price
         except Exception as e:
-            print(f"Error: {e}")
+            print(colored(f"Error: {e}", 'red'))
 
         return False
+    
+    def get_candle(self, symbol, timeframe):
+        # เรียก ข้อมูลราคา ของสินค้ามาไว้ในรูปแบบ ตาราง
+        # กำหนดตัวแปร ที่ต้องใช้
+        # timeframe = ช่วงเวลาของกราฟที่จะดึงข้อมูล 1, 5, 15, 60, 240, 1D
+        timestramp = self.timeserver()
+        form_time = datetime.datetime.fromtimestamp(
+            timestramp) - datetime.timedelta(days=self.API_LIMIT)
+
+        if (timeframe).find("D") < 0:
+            form_time = datetime.datetime.fromtimestamp(
+                timestramp) - datetime.timedelta(minutes=(int(timeframe) * self.API_LIMIT))
+
+        to_date = datetime.datetime.fromtimestamp(timestramp)
+        msg = f"ดึงข้อมูลกราฟ {colored(symbol +'-'+self.API_CURRENCY, 'blue')} จากวันที่ {(colored(form_time, 'blue'))} ถึง {colored(to_date, 'blue')}"
+        print(msg)
+        # # เรียก ข้อมูล จาก  exchange
+        url = f"{self.API_HOST}/tradingview/history?symbol={symbol}_{self.API_CURRENCY}&resolution={timeframe}&from={int((form_time).timestamp())}&to={int((to_date).timestamp())}"
+        response = requests.get(url, headers={}, data={})
+        candles = response.json()
+        # # เรียงให้เป็น ตาราง + เอามาจัดเรียง ใส่หัวข้อ
+        df = pd.DataFrame(
+            candles, columns=['t', 'o', 'c', 'h', 'l', 'v'])
+
+        # # แปลงรูปแบบ ของเวลา ด้วย Pandas
+        df['t'] = pd.to_datetime(df['t'], unit='s')
+        data = df.rename({'t': 'datetime', 'o': 'open',
+                        'c': 'close', 'h': 'high', 'l': 'low', 'v': 'volume'}, axis=1)
+        
+        ### บันทึกข้อมูลเป็น excel
+        export_path = f"exports/excels/assets/{symbol}"
+        if not os.path.exists(export_path):
+            os.makedirs(export_path)
+            
+        filename = f"{export_path}/{timeframe}.{datetime.datetime.now().strftime('%Y%m%d')}.xlsx"
+        data.to_excel(filename)
+        ##  ------>
+        
+        print(data.tail())
+        return data
+    
+    def trand_ema(self, symbol, df, fast_length=9, slow_length=26, export_limit=7, export_full=False):
+        print(f"trend up {fast_length} {slow_length}")
+        # เรียกโมดูล EMS
+        EMA_FAST = df.ta.ema(fast_length)
+        EMA_SLOW = df.ta.ema(slow_length)
+        
+        ## เพิ่มคอลั่ม EMA
+        data = pd.concat([df, EMA_FAST], axis=1)
+        df_ohlcv = pd.concat([data, EMA_SLOW], axis=1)
+        
+        df_length = (len(df_ohlcv) - 1)
+        EMS_FAST_A = df_ohlcv['EMA_' + str(fast_length)][df_length]
+        EMS_FAST_B = df_ohlcv['EMA_' + str(fast_length)][df_length - 1]
+        
+        EMS_SLOW_A = df_ohlcv['EMA_' + str(slow_length)][df_length]
+        EMS_SLOW_B = df_ohlcv['EMA_' + str(slow_length)][df_length - 1]
+        
+        export_path = f"exports/excels/trends"
+        if not os.path.exists(export_path):
+            os.makedirs(export_path)
+            
+        filename = f"{export_path}/EMA.{fast_length}.{slow_length}.{symbol}.{datetime.datetime.now().strftime('%Y%m%d')}.xlsx"
+        if export_full:
+            df_ohlcv.to_excel(filename)
+            
+        else:
+            obj = df_ohlcv.tail(export_limit)
+            obj.to_excel(filename)
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
 from crytoex.bitkub import Bitkub
+from termcolor import colored
 
 from dotenv import load_dotenv
 # Initialize Environment
@@ -8,8 +9,37 @@ load_dotenv()
 bitkub = Bitkub()
 
 def main():
-    balance = bitkub.get_balance_assets()
-    print(list(balance))
+    bal = bitkub.get_balance_assets()
+    if bal[0]['available'] > 10:
+        print('ok')
+        
+    assets = bitkub.get_assets()
+    # i = 0
+    # while i < len(assets):
+    #     r = assets[i]
+    #     id          = r['id']
+    #     info        = r['info']
+    #     symbol      = r['symbol']
+    #     gbal        = r['global']
+    #     currency    = r['currency']
+        
+    #     # print(id,info,symbol,gbal,currency)
+    #     get_price = bitkub.get_last_price(gbal)
+    #     current = 0
+    #     if get_price:
+    #         current = float(get_price['last'])
+
+    #     print(f"""Check Assets({colored(symbol, 'blue')})\nValue: {colored(f'{current:,}', 'yellow')} per {colored(currency, 'blue')}\n{colored(''.rjust(60, '#'), 'yellow')}""")
+    #     obj = bitkub.get_candle(symbol, '1D')
+        
+    #     ### end
+    #     print(f"\n{''.rjust(60, '-')}")
+    #     i += 1
+    
+    symbol = "ETH"
+    obj = bitkub.get_candle(symbol, '1D')
+    trends = bitkub.trand_ema(symbol ,obj, fast_length=7, slow_length=45)
+    
     
 if __name__ == '__main__':
     main()
